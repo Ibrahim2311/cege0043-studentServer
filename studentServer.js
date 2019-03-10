841fd8d58d661843b272912680f8edd6b4dddddd
ibrahim2311 <i.s.h.94@hotmail.com>
2019-03-10 17:45:55 -0400

    serving static files

--- studentServer.js
+++ studentServer.js
@@ -3,13 +3,18 @@ var express = require('express');
 var path = require("path");
 var app = express(); 
 
-
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
 var http = require('http');
 var httpServer = http.createServer(app);
 httpServer.listen(4480);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+}); 
 
 // adding functionality to log the requests
 app.use(function (req, res, next) {
@@ -24,10 +29,6 @@ app.get('/',function (req,res) {
     res.send("hello world from the HTTP server");
 }); 
 
-app.get('/test.html', function (req, res) {
-    // run some server-side code
-    console.log('test.html requested');
-    // note that __dirname gives the path to the studentServer.js file
-    res.sendFile(__dirname + '/test.html');
-});
-
+// serve static files - e.g. html, css
+// this should always be the last line in the server file
+app.use(express.static(__dirname)); 
