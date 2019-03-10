ccebce5a473ac33d6b8b6e560615ae304d359a55
ibrahim2311 <i.s.h.94@hotmail.com>
2019-03-10 17:56:10 -0400

    simple db connection

--- studentServer.js
+++ studentServer.js
@@ -2,7 +2,15 @@
 var express = require('express');
 var path = require("path");
 var app = express(); 
+var fs = require('fs');
+var pg = require('pg'); 
 
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json()); 
 // add an http server to serve files to the Edge browser
 // due to certificate issues it rejects the https files if they are not
 // directly called in a typed URL
@@ -24,7 +32,6 @@ app.use(function (req, res, next) {
     next();
 });
 
-
 app.get('/',function (req,res) {
     res.send("hello world from the HTTP server");
 }); 
@@ -32,3 +39,32 @@ app.get('/',function (req,res) {
 // serve static files - e.g. html, css
 // this should always be the last line in the server file
 app.use(express.static(__dirname)); 
+
+
+var configtext = "" + fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+    var split = configarray[i].split(':');
+    config[split[0].trim()] = split[1].trim();
+}
+var pool = new pg.Pool(config); 
+
+
+app.get('/postgistest', function (req,res) {
+    pool.connect(function(err,client,done) {
+        if(err){
+            console.log("not able to get connection "+ err); 
+            res.status(400).send(err);
+        }
+        client.query('SELECT name FROM london_poi' ,function(err,result) {
+            done();
+            if(err){
+                console.log(err);
+                res.status(400).send(err);
+            }
+            res.status(200).send(result.rows);
+        });
+    });
+});
