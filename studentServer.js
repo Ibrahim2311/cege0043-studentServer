499f6582d94fe72572f8069e6782c94858b3dbea
ibrahim2311 <i.s.h.94@hotmail.com>
2019-03-10 17:16:36 -0400

    create simple middleware

--- studentServer.js
+++ studentServer.js
@@ -5,12 +5,21 @@ var app = express();
 
 
 // add an http server to serve files to the Edge browser
- // due to certificate issues it rejects the https files if they are not
- // directly called in a typed URL
- var http = require('http');
- var httpServer = http.createServer(app);
- httpServer.listen(4480);
+// due to certificate issues it rejects the https files if they are not
+// directly called in a typed URL
+var http = require('http');
+var httpServer = http.createServer(app);
+httpServer.listen(4480);
 
- app.get('/',function (req,res) {
+// adding functionality to log the requests
+app.use(function (req, res, next) {
+    var filename = path.basename(req.url);
+    var extension = path.extname(filename);
+    console.log("The file " + filename + " was requested.");
+    next();
+});
+
+app.get('/',function (req,res) {
     res.send("hello world from the HTTP server");
  }); 
+
